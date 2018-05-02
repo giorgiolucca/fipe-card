103c5311309f08513ba07d80ac78f929cc9dcc76
Giorgio Lucca <giorgiolucca@hotmail.com>
2018-05-01 23:39:28 -0300

    Application entry point and App file

---
+++ README.md
@@ -0,0 +1,1 @@
+## FIPE Card
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'reactstrap'
+import Intro from './containers/Intro'
+import Brand from './containers/Brand'
+import Vehicle from './containers/Vehicle';
+import VehicleTypeChooser from './containers/VehicleTypeChooser'
+import VehicleCard from './containers/VehicleCard'
+import ModelChooser from './containers/ModelChooser';
+
+import { Route } from 'react-router-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Container style={{ paddingTop: '2em' }}>
+                    <Route exact path='/' component={Intro} />
+                    <Route path='/brand/:type' component={Brand} />
+                    <Route path='/vehicle-types' component={VehicleTypeChooser} />
+                    <Route path='/vehicle/:type/:brand' component={Vehicle} />
+                    <Route path='/vehicle-card/:type/:brand/:name/:id/:year' component={VehicleCard} />
+                    <Route path='/vehicle-models/:type/:brand/:id/:name' component={ModelChooser} />
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default App;
