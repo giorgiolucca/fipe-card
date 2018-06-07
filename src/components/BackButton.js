c2250b8377a0d47fdd6299f7f650b3e32d2f35f3
Giorgio Lucca <giorgiolucca@hotmail.com>
2018-06-07 21:54:22 +0000

    Convert BackButton component to stateless

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
-export default class BackButton extends Component {
-    render() {
-        return (
-            <Link to={this.props.route}>
-                <Button color="primary">Voltar</Button>
-            </Link>
-        )
-    }
-}
+const BackButton = props => (
+    <Link to={props.route}>
+        <Button color="primary">{ props.children || 'Voltar' }</Button>
+    </Link>
+)
+
+export default BackButton
